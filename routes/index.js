de97906377d28b69eb4a3fed7143bb611859ba88
t1seal <nk3979603@gmail.com>
2026-03-06 22:26:56 +0200

    order.routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+//Catalog
 const express = require('express');
 const router = express.Router();
 const catalogRouter = require('./catalog.routes');
@@ -6,3 +7,7 @@ const catalogRouter = require('./catalog.routes');
 router.use('/catalog', catalogRouter);
 
 module.exports = router;
+
+//Order
+const orderRouter = require('./order.routes');
+router.use('/orders', orderRouter);
