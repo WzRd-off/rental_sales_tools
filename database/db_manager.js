0ba334eda23c16d0dfc26acfe11dedcb56e827df
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-03-06 19:06:23 +0200

    return dbmanager

--- database/db_manager.js
+++ database/db_manager.js
@@ -1,10 +1,73 @@
-const Sequelize = require('sequelize');
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 
 const DB_PATH = './database/db_main.db';
-    
+
+const db = new sqlite3.Database(DB_PATH, (err) => {
+    if (err) {
+        console.error('Помилка підключення до бази даних:', err.message);
+    } else {
+        console.log('Підключення до SQLite успішно.');
+    }
+});
+
 class DBManager {
+    constructor(database) {
+        this.db = database;
+        this._initSchema();
+    }
 
+    _initSchema() {
+        try {
+            const schemaSql = fs.readFileSync('./database/schema.sql', 'utf8');
 
+            db.exec(schemaSql, (err) => {
+                if (err) {
+                    console.error('Помилка при виконанні schema.sql:', err.message);
+                } else {
+                    console.log('База даних успішно ініціалізована схемою.');
+                }
+            });
+        } catch (err) {
+            console.error('Помилка читання файлу schema.sql:', err.message);
+        }
+    }
+
+    run(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.run(query, params, function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this);
+                }           
+             });
+        });
+    }
+
+    get(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.get(query, params, (err, row) => {              
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }         
+            });  
+        });  
+    }
+
+    all(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(query, params, (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
 }
 
 module.exports = DBManager;
