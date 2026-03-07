c4fae2ea264f1991b007c0e12a7da3e2f7b71eef
Kirll Kozlov <kozlovkirill045@gmail.com>
2026-03-07 23:58:40 +0200

    fix authcontroler + delet login field

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,14 +4,23 @@ const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { email, fullname, password, number } = req.body;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        if (!emailRegex.test(email)){
+            return res.status(500).json({success: false, message: 'Пошта невiрна'});
+        }
+        const numberRegex = /^\+\d{1,3}\d{9,15}$/; 
+        if (!numberRegex.test(number)){
+            return res.status(500).json({success: false, message: 'Номер телефону невiрний'});
+        }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const existingUser = await db._get('SELECT * FROM users WHERE username = ?', [username]);
+        const existingUser = await db._get('SELECT * FROM users WHERE email = ?', [email]);
         if (existingUser) {
             return res.status(400).json({ success: false, message: 'Користувач з таким ім\'ям вже існує' });
         }
-        await db._run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
+        await db._run('INSERT INTO users (email, fullname, password_hash, number) VALUES (?, ?, ?, ?)', [email, fullname, hashedPassword, number]);
         res.status(201).json({ success: true, message: 'Користувача успішно зареєстровано' });
+
     } catch (error) {
         console.error('Помилка при реєстрації користувача:', error);
         res.status(500).json({ success: false, message: 'Помилка сервера' });
@@ -20,11 +29,11 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await db._get('SELECT * FROM users WHERE username = ? AND password = ?', [username, hashedPassword]);
+        const user = await db._get('SELECT * FROM users WHERE email = ? AND password_hashed = ?', [email, hashedPassword]);
         if (!user) {
-            return res.status(401).json({ success: false, message: 'Невірне ім\'я користувача або пароль' });
+            return res.status(401).json({ success: false, message: 'Невірна пошта або пароль' });
         }
         res.status(200).json({ success: true, message: 'Успішний вхід' });
     } catch (error) {
