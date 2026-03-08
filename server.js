0cd1003485b2f5a23e29d1c8ee5dc6580462b2a7
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-03-08 19:07:16 +0200

    merge back+bohdan

--- server.js
+++ server.js
@@ -8,27 +8,6 @@ const PORT = 3000;
 const dbManager = new DBManager();
 const app = express();
 
-
-
-async function changeUserData()
-{
-    let email = document.getElementById('user-email').textContent,
-     login =  document.getElementById('user-login').textContent,
-     fullname = document.getElementById('user-fullname').textContent,
-     password_hash = document.getElementById('user-password').textContent,
-     phone_number = document.getElementById('user-number').textContent;
-
-      try {
-        await dbManager.run(
-            'SELECT * FROM users WHERE id ==  (login, fullname, email, password_hash, phone_number) VALUES (?, ?, ?, ?, ?)', 
-            [`${login}`, `${fullname}`, `${email}`, `${password_hash}`, `${phone_number}`]
-        );
-        console.log('User is added');
-    } catch (err) {
-        console.error('Error:', err.message);
-    }
-}
-
 app.use(express.json());
 app.use(cors());
 app.use('/api', apiRouter);
